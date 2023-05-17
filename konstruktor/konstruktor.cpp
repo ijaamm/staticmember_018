2f10615767083934e7442a00e05655e05795d63f
ijaamm <ijammusyaf@gmail.com>
2023-05-17 17:04:57 +0700

    done

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -35,3 +35,17 @@ void mahasiswa::cetak() {
 	cout << endl << "nim = " << endl;
 	cout << "nama = " << nama << endl;
 }
+
+int main() {
+	mahasiswa mhs1;
+	mahasiswa mhs2(20);
+	mahasiswa mhs3("indra");
+	mahasiswa mhs4(30, "fauzan");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
+}

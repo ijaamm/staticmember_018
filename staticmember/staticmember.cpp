e07c4076819651506697476ec0c9f013bfb148e5
ijaamm <ijammusyaf@gmail.com>
2023-05-17 15:42:04 +0700

    int main mahasiswa

--- staticmember/staticmember.cpp
+++ staticmember/staticmember.cpp
@@ -25,3 +25,19 @@ void mahasiswa::printaALL() {
 	cout << "NIM = " << nim << endl;
 	cout << endl;
 }
+
+int main() {
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mhs2.nim = 10;
+	mahasiswa mhs3("Andi Kurniawan");
+	mhs3.nim = 20;
+	mahasiswa mhs4("Joko Purbo");
+
+	mhs1.printaALL();
+	mhs2.printaALL();
+	mhs3.printaALL();
+	mhs4.printaALL();
+
+	return 0;
+}
